647c30481bf146edd257b75a79096b9012841e0b
Manu BuddHa <ferano915@gmail.com>
2026-04-22 01:24:24 +0530

    Initialised DB Connection

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require('express');
+require('dotenv').config();
+const connectDB = require('./config/db');
+connectDB();
 const app = express();
 const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
