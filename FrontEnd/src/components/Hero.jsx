cb37575eb272317e76d7d42948bf8be970971e2f
Manu BuddHa <ferano915@gmail.com>
2026-05-14 04:06:29 +0530

    Login Page Design

--- FrontEnd/src/components/Hero.jsx
+++ FrontEnd/src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Container = styled.div`
     `;
 
 const Mask = styled.div`
-    background-color: rgba(0,0,0, 0.5);
+    background-color: rgba(0,0,0, 0.3);
     width: 100%;
     height: 100%;
     display: flex;
--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -1,58 +1,107 @@
 import styled from "styled-components";
-import img from '../images/login.jpg'
+import img from '../images/SignupBG.jpg'
+
+const BGimg = styled.div`
+    background-image: url(${img});
+    background-position: center bottom;
+    background-size: cover;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+`;
 
 const Container = styled.div`
-    
-    width: 80%;
-    background-image: url(${img});
-    background-position: left bottom;
-    background-size: cover;
-    margin: 2em auto;
-    box-shadow: 0 0 12px rgba(0, 0, 0,0.5);
-    
-`;
-const Opacitydiv = styled.div`
+    width: 30%;
+    margin: auto;
+    border: 2px solid #ffffff;
+    border-radius: 12px;
+    box-shadow: 0 0 12px rgba(255, 255, 255,0.5);
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin: 2em auto;
-    padding: 2em 0;
-    background-color: #0000007b;
-    height: 100%;
+    padding: 2em;
+    background-color: rgba(0, 0, 0, 0.5);
 `;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 2em;
+    width: 90%;
+    margin: 3em auto;
+    `;
 const FormDiv = styled.div`
     display: flex;
     align-items: center;
     font-family: Montserrat;
+    gap: 1em;
     font-size: 1.2em;
     justify-content: space-between;
     width: 100%;
     text-shadow: 2px 2px 6px black;
     color: #ffffff;
     font-weight: 500;
+    margin: auto;
+    & label {
+        width: 25%;
+        text-align: right;
+    }
 `;
+const LoginBtn = styled.button`
+    margin: 1em auto;
+    padding: 0.5em;
+    font-size: 1.2em;
+    background-color: #2b8aaa;
+    color: white;
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+    width: 80%;
+    font-family: Montserrat;
+    font-weight: 600;
+    &:hover{
+        transform: scale(1.05);
+        background-color: #0091ff;
+        box-shadow: 0 0px 6px rgba(255, 255, 255, 0.3);
+        transition: all 0.3s ease;
+    }
+`
+const Input = styled.input`
+    padding: 0.5em;
+    font-size: 1.2em;
+    width: 65%;
+    background-color: transparent;
+    border: none;
+    border-bottom: 3px solid #0048ff;
+    color: #ffffff;
+    &:focus {
+        outline: none;
+        border-bottom: 3px solid #0091ff;
+        transition: all 0.3s ease;
+    }
+`;
+
+
 function Login() {
     return (
-        <div>
+        <BGimg>
             <Container>
-                <Opacitydiv>
                     <u style={{ color: 'red' }}><h1 className="heading">Login</h1></u>
-                    <form>
+                    <Form>
                         <FormDiv>
                             <label htmlFor="username">Username:</label>
-                            <input type="text" id="username" placeholder="Username" style={{ padding: '0.5em', fontSize: '1.2em', width: '80%' }} />
+                            <Input type="text" id="username" placeholder="Username" />
                         </FormDiv>
                         <FormDiv>
                             <label htmlFor="password">Password:</label>
-                            <input type="password" id="password" placeholder="Password" style={{ padding: '0.5em', fontSize: '1.2em', width: '80%' }} />
+                            <Input type="password" id="password" placeholder="Password" />
                         </FormDiv>
                         <FormDiv>
-                            <button type="submit" className="submitBtn">Login</button>
+                            <LoginBtn type="submit">Login</LoginBtn>
                         </FormDiv>
-                    </form>
-                </Opacitydiv>
+                    </Form>
             </Container>
-        </div>
+        </BGimg>
     )
 }
 
